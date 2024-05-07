41e6679951f4a3de993fda9dd1177d3c99165b76
ELTemi <ruth_olukunle@yahoo.com>
2024-05-07 15:45:52 -0600

    disable button when all topics have been generated

--- app/group5/page.js
+++ app/group5/page.js
@@ -28,7 +28,7 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center h-screen">
     <h1 className="text-3xl font-bold mb-6">Random Web Builder Topic Generator</h1>
     <p className="text-lg mb-6">{topic}</p>
-    <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-300 ease-in-out" onClick={generateRandomTopic}>Generate</button>
+    <button className={`px-4 py-2 ${topics.length === 0 ?  "bg-gray-400" : "bg-blue-500"} text-white rounded ${topics.length === 0 ?  "bg-gray-400" : "hover:bg-blue-600"} transition duration-300 ease-in-out`} disabled={topics.length === 0} onClick={generateRandomTopic}>Generate</button>
   </div>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,39 +1,27 @@
 'use client'
 import { useState } from 'react';
 
-const topics1 = [
+const topics = [
   "Responsive Design",
   "User Interface (UI)",
   "Cookies",
   "Mobile Optimization",
   "Call to Action (CTA)",
-];
-
-const topics2 = [
   "Responsive Design",
   "API (Application Programming Interface)",
   "Navigation Bar",
   "Custom Code",
   "User Interface (UI)",
-];
-
-const topics3 = [
   "User Experience (UX)",
   "Wireframe",
   "SSL (Secure Sockets Layer)",
   "Web Builder",
   "SEO (Search Engine Optimization)",
-];
-
-const topics4 = [
   "Cookies",
   "JavaScript",
   "HTML5",
   "CSS",
   "Database",
-];
-
-const topics5 = [
   "Web Hosting",
   "Mobile Optimization",
   "Call to Action (CTA)",
@@ -41,17 +29,16 @@ const topics5 = [
   "Analytics",
 ];
 
-
 export default function Home() {
   const [topic, setTopic] = useState('');
 
-  const generateRandomTopic1 = () => {
-    if (topics1.length === 0) {
+  const generateRandomTopic = () => {
+    if (topics.length === 0) {
       setTopic('No more topics left!');
     } else {
-      const randomIndex = Math.floor(Math.random() * topics1.length);
-      const randomTopic = topics1[randomIndex];
-      topics1.splice(randomIndex, 1);
+      const randomIndex = Math.floor(Math.random() * topics.length);
+      const randomTopic = topics[randomIndex];
+      topics.splice(randomIndex, 1);
       setTopic(randomTopic);
     }
   };
@@ -60,7 +47,7 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center h-screen">
     <h1 className="text-3xl font-bold mb-6">Random Topic Generator</h1>
     <p className="text-lg mb-6">{topic}</p>
-    <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-300 ease-in-out" onClick={generateRandomTopic1}>Group 1</button>
+    <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-300 ease-in-out" onClick={generateRandomTopic}>Generate</button>
   </div>
   );
 }
